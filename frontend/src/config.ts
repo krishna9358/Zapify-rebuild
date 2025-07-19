526ff0b4d2654dbe2eda648a584682928f35d6ce
Krishna Mohan <krishanmohank974@gmail.com>
2025-07-19 09:35:37 +0530

    fixed build command, config for frontend deoployment

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,3 @@
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = process.env.NODE_ENV === "production" 
+  ? "https://zapify-rebuild.onrender.com" 
+  : "http://localhost:3000";
